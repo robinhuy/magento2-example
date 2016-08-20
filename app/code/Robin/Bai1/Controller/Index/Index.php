ee091ec17e3c36238b02db1e17bf59458b5efb24
Robin Huy <robin@techmaster.vn>
2016-08-20 15:51:48 +0700

    Layout, Block và Template :newspaper:

--- app/code/Robin/Bai1/Controller/Index/Index.php
+++ app/code/Robin/Bai1/Controller/Index/Index.php
@@ -2,11 +2,22 @@
 
 namespace Robin\Bai1\Controller\Index;
 
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
 class Index extends \Magento\Framework\App\Action\Action {
+
+    protected $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        parent::__construct($context);
+    }
 
     public function execute()
     {
-        echo "Hello World";
+        return $this->pageFactory->create();
     }
 
 }
