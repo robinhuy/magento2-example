3bf2be2c2ae2e391ddcd3962b6eaf9002c794f7a
Robin Huy <robin@techmaster.vn>
2016-08-20 15:42:29 +0700

    Module Hello World :smiley:

---
+++ app/code/Robin/Bai1/Controller/Index/Index.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Robin\Bai1\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action {
+
+    public function execute()
+    {
+        echo "Hello World";
+    }
+
+}
