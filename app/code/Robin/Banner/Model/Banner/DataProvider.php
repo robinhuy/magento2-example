90943332a0e11fa88b43bd22297d0944f85823ad
Robin Huy <robin@techmaster.vn>
2016-08-22 16:36:21 +0700

    Module Quản lý Banner phần 6 - Form

---
+++ app/code/Robin/Banner/Model/Banner/DataProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Robin\Banner\Model\Banner;
+
+use Robin\Banner\Model\ResourceModel\Banner\CollectionFactory;
+use Magento\Framework\App\Request\DataPersistorInterface;
+
+class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
+{
+
+    protected $collection;
+    protected $dataPersistor;
+    protected $loadedData;
+
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $bannerCollectionFactory,
+        DataPersistorInterface $dataPersistor,
+        array $meta = [],
+        array $data = []
+    )
+    {
+        $this->collection = $bannerCollectionFactory->create();
+        $this->dataPersistor = $dataPersistor;
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+        $this->meta = $this->prepareMeta($this->meta);
+    }
+
+    /**
+     * Prepares Meta
+     */
+    public function prepareMeta(array $meta)
+    {
+        return $meta;
+    }
+
+    /**
+     * Get data
+     */
+    public function getData()
+    {
+        // Get items
+        if (isset($this->loadedData)) {
+            return $this->loadedData;
+        }
+        $items = $this->collection->getItems();
+
+        
+        foreach ($items as $banner) {
+            $this->loadedData[$banner->getId()] = $banner->getData();
+        }
+
+        $data = $this->dataPersistor->get('banner');
+        if (!empty($data)) {
+            $banner = $this->collection->getNewEmptyItem();
+            $banner->setData($data);
+            $this->loadedData[$banner->getId()] = $banner->getData();
+            $this->dataPersistor->clear('banner');
+        }
+
+        return $this->loadedData;
+    }
+}
